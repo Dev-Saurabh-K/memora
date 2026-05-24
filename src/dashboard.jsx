e306c405ee974d7c5fc852f503893c7ca11484f6
Rajnishant <rajnishant87827049@gmail.com>
2026-05-24 15:12:16 +0530

    logo change

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,10 +1,11 @@
  import React from 'react';
  import {useState} from 'react';
  import { Link } from "react-router-dom";
- import memoraLogo from './assets/logo.svg';
+ 
+ import Logo from './logo'; 
 
 export default function Dashboard() {
-    const [selectedClass, setClasses] = useState(5);
+    const [selectedClass, setClasses] = useState(5); 
     const classes = [5,6,7,8,9,10,11,12];
     const handleClick = () => {
       if (selectedClass) {
@@ -18,16 +19,13 @@ export default function Dashboard() {
                 <div className="space-y-3">
                  <h1 className="text-3xl font-semibold flex items-center justify-center ">
                   <span>Welcome to</span>
-                  <img 
-    src={memoraLogo} 
-    alt="Memora Logo" 
-    className="h-11 w-auto object-contain translate-y-0.5" 
-  />
+                  <Logo className="h-11 ml-2 w-auto" />
                  </h1>
-                 <p className="">To personalize your learning journey, please select your grade level.
+
+                 <p className="text-[#c8c8c8] text-sm md:text-base font-normal">To personalize your learning journey, please select your grade level.
                  </p>
                 </div>
-                <div className= "grid grid-cols-2 gap-4 sm:grid-cols-4 pt-4" >
+                <div className= "grid grid-cols-2 gap-4 sm:grid-cols-4 pt-4 max-w-4xl mx-auto px-4 sm:px-6 md:px-8 lg:px-10">
                      {classes.map((grade, index) => {
                         const isSelected = selectedClass === grade;
                         return (
@@ -50,7 +48,7 @@ export default function Dashboard() {
                       <Link to="/page2">
                      <button  
                      onClick={handleClick}
-                     className="bg-[#2d533c] hover:bg-[#419360] text-[#ccc1c1] 
+                     className="bg-[#245f3b] hover:bg-[#419360] text-[#000000] 
                      font-bold py-3 px-12 rounded-xl text-base tracking-wide transition-colors shadow-lg" >Continue</button>
                      </Link>
                     </div>

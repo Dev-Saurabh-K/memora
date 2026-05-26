0c0e860144340667ae239138fe6d462496b1fe59
Dev-Saurabh-K <saurabhkumar.sakr@gmail.com>
2026-05-26 15:39:27 +0530

    build fix

--- src/Pages/chat/chatLayout.jsx
+++ src/Pages/chat/chatLayout.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import Sidebar from "../../pages/page2/Sidebar";
-import Navbar from "../../pages/page2/Navbar";
-import Logo from "../../logo";
+import { useState } from 'react';
+import Sidebar from "../../Pages/Page2/Sidebar";
+import Navbar from "../../Pages/Page2/Navbar";
+// import Logo from "../../logo";
 
 export default function ChatLayout() {
 

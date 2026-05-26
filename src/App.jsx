446c9594f455b8de8c39a5d7d3a3635ddc40ac24
Dev-Saurabh-K <saurabhkumar.sakr@gmail.com>
2026-05-27 00:29:59 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,9 @@ import './App.css'
 import Dashboard from "./dashboard";
 import Page2 from './Pages/Page2/page2'
 import Page3 from "./Pages/Page3/page3";
-
-function App() {
-
-  return (
-    // <Routes> 
-    // <Route path="/" element={<Dashboard />} />
-    // <Route path="/page2" element={<Page2 />} />
-    // </Routes>
-    <div className='w-screen h-screen bg-black'><h1>kk</h1>
-    <Page3/>
-    </div>
 import Chat from './Pages/chat/chatLayout'
 import Home from './Pages/homePage/home'
+
 function App() {
 
   return (

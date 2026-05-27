78711e91a720b37a79914bb785da7624ed4fd065
Dev-Saurabh-K <saurabhkumar.sakr@gmail.com>
2026-05-27 21:47:13 +0530

    login signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { Routes, Route } from "react-router-dom";
-import './App.css'
+
+import Login from "./Pages/Authentication/Login"
 import Dashboard from "./dashboard";
 import Page2 from './Pages/Page2/page2'
 import Page3 from "./Pages/Page3/page3";
 import Chat from './Pages/chat/chatLayout'
 import Home from './Pages/homePage/home'
+import Signup from "./Pages/Authentication/Signup";
 
 function App() {
 
   return (
     <Routes> 
     <Route path="/" element={<Dashboard />} />
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/signup" element={<Signup/>}/>
+    <Route path="/page3" element={<Page3/>}/>
     <Route path="/page2" element={<Page2 />} />
     <Route path="/chat" element={<Chat />} />
     <Route path="/home" element={<Home />} />
+
     </Routes>
   )
 }
---
+++ src/Pages/Authentication/Logo.jsx
@@ -0,0 +1,12 @@
+
+
+const Logo = ()=>{
+
+    return(
+        <div className="text-[#10B981]  font-sans text-8xl font-bold fixed top-4">
+            memora
+        </div>
+    )
+}
+
+export default Logo;
